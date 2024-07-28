2909feadd3e8580d50c07679d29d55b2d0b7681d
Alok Singh <aloksingh15122004@gmail.com>
2024-07-28 10:15:50 +0530

    completed

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const isLogged=(req,res,next)=>{
     if(!req.isAuthenticated()){
-      req.flash("error","You must be logged in");
+      req.flash("error","You are required to logged in");
       res.redirect("/login");
     }else
     {
